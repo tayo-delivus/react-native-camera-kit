e4728510a71c1659224f27811e572a6478548c65
tayo-delivus <tayo@delivus.co.kr>
2025-02-28 15:56:51 +0900

    Update QRCodeAnalyzer.kt

--- android/src/main/java/com/rncamerakit/QRCodeAnalyzer.kt
+++ android/src/main/java/com/rncamerakit/QRCodeAnalyzer.kt
@@ -1,6 +1,7 @@
 package com.rncamerakit
 
 import android.annotation.SuppressLint
+import android.util.Size
 import androidx.camera.core.ExperimentalGetImage
 import androidx.camera.core.ImageAnalysis
 import androidx.camera.core.ImageProxy
@@ -9,11 +10,12 @@ import com.google.mlkit.vision.barcode.common.Barcode
 import com.google.mlkit.vision.common.InputImage
 
 class QRCodeAnalyzer (
-    private val onQRCodesDetected: (qrCodes: List<Barcode>) -> Unit
+    private val onQRCodesDetected: (qrCodes: List<Barcode>, imageSize: Size) -> Unit
 ) : ImageAnalysis.Analyzer {
     @SuppressLint("UnsafeExperimentalUsageError")
     @ExperimentalGetImage
     override fun analyze(image: ImageProxy) {
+        val mediaImage = image.image ?: return
         val inputImage = InputImage.fromMediaImage(image.image!!, image.imageInfo.rotationDegrees)
 
         val scanner = BarcodeScanning.getClient()
@@ -23,7 +25,7 @@ class QRCodeAnalyzer (
                 barcodes.forEach { barcode ->
                     strBarcodes.add(barcode ?: return@forEach)
                 }
-                onQRCodesDetected(strBarcodes)
+                onQRCodesDetected(strBarcodes, Size(image.width, image.height))
             }
             .addOnCompleteListener{
                 image.close()
