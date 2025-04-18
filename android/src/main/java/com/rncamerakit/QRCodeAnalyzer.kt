3e395042743f85775a628ce2a213c523d48b1302
jihunjeong <tayo@delivus.co.kr>
2025-04-18 13:24:41 +0900

    fix: fix frame boundary

--- android/src/main/java/com/rncamerakit/QRCodeAnalyzer.kt
+++ android/src/main/java/com/rncamerakit/QRCodeAnalyzer.kt
@@ -1,32 +1,35 @@
 package com.rncamerakit
 
-import android.annotation.SuppressLint
-import androidx.camera.core.ExperimentalGetImage
-import androidx.camera.core.ImageAnalysis
-import androidx.camera.core.ImageProxy
-import com.google.mlkit.vision.barcode.BarcodeScanning
-import com.google.mlkit.vision.barcode.common.Barcode
-import com.google.mlkit.vision.common.InputImage
+   import android.annotation.SuppressLint
+   import android.util.Size
+   import androidx.camera.core.ExperimentalGetImage
+   import androidx.camera.core.ImageAnalysis
+   import androidx.camera.core.ImageProxy
+   import com.google.mlkit.vision.barcode.BarcodeScanning
+   import com.google.mlkit.vision.barcode.common.Barcode
+   import com.google.mlkit.vision.common.InputImage
 
-class QRCodeAnalyzer (
-    private val onQRCodesDetected: (qrCodes: List<Barcode>) -> Unit
-) : ImageAnalysis.Analyzer {
-    @SuppressLint("UnsafeExperimentalUsageError")
-    @ExperimentalGetImage
-    override fun analyze(image: ImageProxy) {
-        val inputImage = InputImage.fromMediaImage(image.image!!, image.imageInfo.rotationDegrees)
+   class QRCodeAnalyzer (
+       private val onQRCodesDetected: (qrCodes: List<Barcode>, imageSize: Size) -> Unit
+   ) : ImageAnalysis.Analyzer {
+       @SuppressLint("UnsafeExperimentalUsageError")
+       @ExperimentalGetImage
+       override fun analyze(image: ImageProxy) {
+           val mediaImage = image.image ?: return
 
-        val scanner = BarcodeScanning.getClient()
-        scanner.process(inputImage)
-            .addOnSuccessListener { barcodes ->
-                val strBarcodes = mutableListOf<Barcode>()
-                barcodes.forEach { barcode ->
-                    strBarcodes.add(barcode ?: return@forEach)
-                }
-                onQRCodesDetected(strBarcodes)
-            }
-            .addOnCompleteListener{
-                image.close()
-            }
-    }
-}
+           val inputImage = InputImage.fromMediaImage(mediaImage, image.imageInfo.rotationDegrees)
+
+           val scanner = BarcodeScanning.getClient()
+           scanner.process(inputImage)
+               .addOnSuccessListener { barcodes ->
+                   val strBarcodes = mutableListOf<Barcode>()
+                   barcodes.forEach { barcode ->
+                       strBarcodes.add(barcode ?: return@forEach)
+                   }
+                   onQRCodesDetected(strBarcodes, Size(image.width, image.height))
+               }
+               .addOnCompleteListener {
+                   image.close()
+               }
+       }
+   }
