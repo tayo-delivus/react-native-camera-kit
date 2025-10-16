8b38ed6840ce2d4231e8c2eb212871a83313e40a
tayo-delivus <tayo@delivus.co.kr>
2025-10-16 17:19:44 +0900

    Update scanner rect handling in CameraView

--- ios/ReactNativeCameraKit/CameraView.swift
+++ ios/ReactNativeCameraKit/CameraView.swift
@@ -186,6 +186,9 @@ public class CameraView: UIView {
         // If frame size changes, we have to update the scanner
         camera.update(scannerFrameSize: showFrame ? scannerInterfaceView.frameSize : nil)
 
+        let rectInPreview = showFrame ? scannerInterfaceView.frameSize : nil
+        camera.updateScannerRectInPreview(rectInPreview, restrictToFrame: scanBarcode)
+        
         focusInterfaceView.frame = bounds
 
         ratioOverlayView?.frame = bounds
@@ -265,6 +268,8 @@ public class CameraView: UIView {
                 self.scannerInterfaceView.isHidden = !self.showFrame
 
                 self.camera.update(scannerFrameSize: self.showFrame ? self.scannerInterfaceView.frameSize : nil)
+                self.camera.updateScannerRectInPreview(self.showFrame ? self.scannerInterfaceView.frameSize : nil,
+                                           restrictToFrame: self.scanBarcode)
             }
         }
         
@@ -272,6 +277,7 @@ public class CameraView: UIView {
             if let width = barcodeFrameSize["width"] as? CGFloat, let height = barcodeFrameSize["height"] as? CGFloat {
                 scannerInterfaceView.update(frameSize: CGSize(width: width, height: height))
                 camera.update(scannerFrameSize: showFrame ? scannerInterfaceView.frameSize : nil)
+                camera.updateScannerRectInPreview(scannerInterfaceView.frameSize, restrictToFrame: true)
             }
         }
 
