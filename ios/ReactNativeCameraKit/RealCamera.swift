c48db8d6d451df8bce2f583fd9a54baedbff741f
tayo-delivus <tayo@delivus.co.kr>
2025-10-17 17:09:54 +0900

    Refactor barcode scanner enabling logic

--- ios/ReactNativeCameraKit/RealCamera.swift
+++ ios/ReactNativeCameraKit/RealCamera.swift
@@ -58,6 +58,7 @@ class RealCamera: NSObject, CameraProtocol, AVCaptureMetadataOutputObjectsDelega
     private var inProgressPhotoCaptureDelegates = [Int64: PhotoCaptureDelegate]()
 
     private var scannerFrameLayerRect: CGRect?
+    private var lastTypes: [AVMetadataObject.ObjectType] = []
 
     // MARK: - Lifecycle
     
@@ -375,25 +376,59 @@ class RealCamera: NSObject, CameraProtocol, AVCaptureMetadataOutputObjectsDelega
         }
     }
 
-    func isBarcodeScannerEnabled(_ isEnabled: Bool,
-                                 supportedBarcodeTypes supportedBarcodeType: [CodeFormat],
-                                 onBarcodeRead: ((_ barcode: String,_ codeFormat:CodeFormat) -> Void)?) {
-        sessionQueue.async {
-            self.onBarcodeRead = onBarcodeRead
-            let newTypes: [AVMetadataObject.ObjectType]
-            if isEnabled && onBarcodeRead != nil {
-                let availableTypes = self.metadataOutput.availableMetadataObjectTypes
-                newTypes = supportedBarcodeType.map { $0.toAVMetadataObjectType() }
-                                                        .filter { availableTypes.contains($0) }
-            } else {
-                newTypes = []
-                self.metadataOutput.rectOfInterest = CGRect(x: 0, y: 0, width: 1, height: 1)
-            }
+    // func isBarcodeScannerEnabled(_ isEnabled: Bool,
+    //                              supportedBarcodeTypes supportedBarcodeType: [CodeFormat],
+    //                              onBarcodeRead: ((_ barcode: String,_ codeFormat:CodeFormat) -> Void)?) {
+    //     sessionQueue.async {
+    //         self.onBarcodeRead = onBarcodeRead
+    //         let newTypes: [AVMetadataObject.ObjectType]
+    //         if isEnabled && onBarcodeRead != nil {
+    //             let availableTypes = self.metadataOutput.availableMetadataObjectTypes
+    //             newTypes = supportedBarcodeType.map { $0.toAVMetadataObjectType() }
+    //                                                     .filter { availableTypes.contains($0) }
+    //         } else {
+    //             newTypes = []
+    //             self.metadataOutput.rectOfInterest = CGRect(x: 0, y: 0, width: 1, height: 1)
+    //         }
 
-            if self.metadataOutput.metadataObjectTypes != newTypes {
-                self.metadataOutput.metadataObjectTypes = newTypes
+    //         if self.metadataOutput.metadataObjectTypes != newTypes {
+    //             self.metadataOutput.metadataObjectTypes = newTypes
+    //         }
+    //     }
+    // }
+
+    func isBarcodeScannerEnabled(_ isEnabled: Bool,
+                             supportedBarcodeTypes supportedBarcodeType: [CodeFormat],
+                             onBarcodeRead: ((_ barcode: String,_ codeFormat:CodeFormat) -> Void)?) {
+      sessionQueue.async {
+        self.onBarcodeRead = onBarcodeRead
+        let available = self.metadataOutput.availableMetadataObjectTypes
+        let types = supportedBarcodeType.map { $0.toAVMetadataObjectType() }.filter { available.contains($0) }
+        self.lastTypes = types
+    
+        if isEnabled && onBarcodeRead != nil {
+          // 연결/방향/ROI를 먼저 확정
+          DispatchQueue.main.async {
+            // 연결이 꺼져 있으면 다시 켜기
+            self.cameraPreview.previewLayer.connection?.isEnabled = true
+            self.metadataOutput.connections.forEach { $0.isEnabled = true }
+    
+            // 인터페이스 방향 재동기화 (특히 display:none → 표시 직후)
+            self.setVideoOrientationToInterfaceOrientation()
+    
+            // ROI는 update(scannerFrame:)에서 이미 설정됨 (여기선 그대로 사용)
+            // 다음 런루프에 타입을 넣어 캡처 파이프라인이 준비된 뒤 적용
+            DispatchQueue.main.async {
+              self.metadataOutput.metadataObjectTypes = types
             }
+          }
+        } else {
+          // 끌 때는 타입만 비우면 OK (ROI는 유지)
+          DispatchQueue.main.async {
+            self.metadataOutput.metadataObjectTypes = []
+          }
         }
+      }
     }
 
     func update(barcodeFrameSize: CGSize?) {
