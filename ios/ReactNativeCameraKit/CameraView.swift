2a01529b197da949a74a0b17c118f61e9f89fc3e
tayo-delivus <tayo@delivus.co.kr>
2025-10-17 17:09:57 +0900

    Update CameraView.swift

--- ios/ReactNativeCameraKit/CameraView.swift
+++ ios/ReactNativeCameraKit/CameraView.swift
@@ -322,7 +322,7 @@ public class CameraView: UIView {
                 self.camera.update(scannerFrame: showFrame ? r1 : nil)
                 DispatchQueue.main.async {
                     let r2 = self.scannerInterfaceView.convert(self.scannerInterfaceView.frameOfInterest, to: self.camera.previewView)
-                    self.camera.update(scannerFrame: showFrame ? r2 : nil)
+                    self.camera.update(scannerFrame: self.showFrame ? r2 : nil)
                 }
             }
         }
@@ -536,4 +536,45 @@ public class CameraView: UIView {
             camera.zoomPinchChange(pinchScale: pinchRecognizer.scale)
         }
     }
+
+    @objc public func setScanningActive(_ active: Bool) {
+      if active {
+        // 1) 레이아웃 고정 & ROI 먼저 적용
+        DispatchQueue.main.async {
+          self.layoutIfNeeded()
+          self.scannerInterfaceView.layoutIfNeeded()
+    
+          let rect1 = self.showFrame
+            ? self.scannerInterfaceView.convert(self.scannerInterfaceView.bounds, to: self.camera.previewView)
+            : nil
+          self.camera.update(scannerFrame: rect1)
+    
+          // 2) 다음 런루프에서 타입 ON (ROI 적용 이후)
+          DispatchQueue.main.async {
+            self.camera.isBarcodeScannerEnabled(
+              true,
+              supportedBarcodeTypes: self.supportedBarcodeType,
+              onBarcodeRead: { [weak self] (barcode, codeFormat) in
+                self?.onBarcodeRead(barcode: barcode, codeFormat: codeFormat)
+              }
+            )
+          }
+        }
+      } else {
+        // 숨길 때는 곧바로 OFF
+        self.camera.isBarcodeScannerEnabled(false, supportedBarcodeTypes: [], onBarcodeRead: nil)
+      }
+    }
+    
+    @objc public func updateScannerFrame() {
+      DispatchQueue.main.async {
+        self.layoutIfNeeded()
+        self.scannerInterfaceView.layoutIfNeeded()
+        let rect = self.showFrame
+          ? self.scannerInterfaceView.convert(self.scannerInterfaceView.bounds, to: self.camera.previewView)
+          : nil
+        self.camera.update(scannerFrame: rect)
+      }
+    }
+
 }
